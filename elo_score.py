f93354ed6d13189664b13fa2fc7dc0247e41be78
mikkoim <mikko.impio@gmail.com>
2017-12-10 01:59:27 +0200

    Funktioiden päivittelyä
    Kykenee laskemaan myös yhden ottelun päähän tehtäviä ennusteita. Lisätty
    metodeja Playersiin, sekä muutama apufunktio. Kommentteja ja siistitty
    koodia

--- elo_score.py
+++ elo_score.py
@@ -2,11 +2,19 @@
 """
 Created on Fri Dec  8 20:13:25 2017
 
-@author: Mikko
+@author: Mikko Impiö
+
+
+!! Ohjelma olettaa, että pelitiedot sisältävät muuttujat 
+kausi1415, kausi1516, kausi1617 ja kausi1718 
+on luotu edellisten scraper.py- skriptin avulla
+!!
+
+"""
 
 
 
-
+"""
 Pelaaja-luokka sisältää yksittäisen pelaajan tiedot:
     Nimen ja ELO-luvun
 """
@@ -32,19 +40,23 @@ class Players:
         self.__playerlist = []
         self.__playernames = []
         self.__length = 0
-        
+            
+    #palauttaa listan pelaajaolioista
     def getPlayers(self):
         return self.__playerlist
-    
+        
+    #palauttaa listan pelaajien nimistä
     def getNames(self):
         return self.__playernames
     
+    #palauttaa listan pelaajien ELO-luvuista
     def getELOs(self):
         ELOs = []
         for player in self.__playerlist:
             ELOs.append(player.getELO())
         return ELOs
     
+    #laskee ja palauttaa pelaajalistassa olevien pelaajien ELO:jen keskiarvot
     def ELOmean(self):
         ELO_sum = 0
         for player in self.__playerlist:
@@ -52,24 +64,29 @@ class Players:
             ELO_sum = ELO_sum + ELO
         return ELO_sum/len(self.__playerlist)
     
+    #palauttaa tietyn pelaajan ELO-luvun
     def playerELO(self, player_name):
         for player in self.__playerlist:
             if player.getNimi() == player_name:
                 return player.getELO()
     
+    #lisää pelaajaolion pelaajalistaan
     def addPlayer(self, Pelaaja):
         self.__playerlist.append(Pelaaja)
         self.__playernames.append(Pelaaja.getNimi())
         
+    #muuttaa KAIKKIEN listassa olevien pelaajien ELO-lukua
     def changeELO(self, ELO_change):
         for player in self.__playerlist:
             orig_ELO = player.getELO()
             player.updateELO(orig_ELO + ELO_change)
             
+    #Nollaa KAIKKIEN listassa olevien pelaajien ELO-luvun valittuun lukuun
     def resetELO(self, ELO=1500):
         for player in self.__playerlist:
             player.updateELO(ELO)
             
+    #Palauttaa pelaajalistan ELO-luvun mukaisessa järjestyksessä
     def getSorted(self, reverse=False):
         sortedPlayers = Players()
         indices = sorted(range(len(self.getELOs())),key=lambda x:self.getELOs()[x], reverse = reverse)
@@ -93,10 +110,15 @@ def sorted_dict(dictionary, reverse = True):
         score = scores[i]
         for joukkue, pisteet in dictionary.items():
             if pisteet == score:
-                print(joukkue, pisteet)
+                print("{:} {:.2f}".format(joukkue, pisteet))
         if ind == 5:
             print("------")
       
+"""
+Kerää tietyn pelin pelaajat ja laskee näiden perusteella ELO-luvun koko
+joukkueelle. Palauttaa listat koti- ja vierasjoukkueesta, sekä näiden
+ELO-keskiarvot
+"""
 def team_ELOs(game):
     home = Players()
     home_player_names = game[0][2]
@@ -119,10 +141,62 @@ def team_ELOs(game):
             if player.getNimi() == player_name:
                 away.addPlayer(player)
                 
+    #ELO-keskiarvot
     R_a = home.ELOmean()
     R_b = away.ELOmean()
     
     return home, away, R_a, R_b
+    
+"""
+Laskee jokaiselle kaudella pelaavalle joukkueelle ELO-luvun sen perusteella,
+ketä joukkueessa tulee pelaamaan tällä kaudella. Käytetään mallissa, sillä
+ei voida tietää ennen kauden alkua varmoja kokoonpanoja otteluille
+"""
+def team_season_ELOs(kausi, p):
+    #Tehdään lista joukkueista
+    joukkueet = {}
+
+    for game in kausi:
+        for team in game[0:1]:
+            team_name = team[0]
+            joukkueet[team_name] = Players()
+    
+    #Käydään pelit läpi
+    for game in kausi:
+        for team in game[0:1]:
+            
+            team_name = team[0]
+
+            #Käydään pelaajien nimet läpi joukkueittain, ja lisätään ne
+            #joukkueen Players-olioon, jos ko. pelaajaa ei siinä jo ole
+            team_player_names = team[2]
+            for player_name in team_player_names:
+                if player_name not in joukkueet[team_name].getNames():
+                    team_players = joukkueet[team_name]
+
+                    for player in p.getPlayers():
+                        if player.getNimi() == player_name:
+                            team_players.addPlayer(player)
+                    
+    #Käydään vielä joukkuet läpi ja lasketaan ELO-keskiarvot
+    for joukkue in joukkueet.keys():
+        joukkueet[joukkue] = joukkueet[joukkue].ELOmean()
+        
+    return joukkueet
+        
+    
+"""
+Luo joukkueet- dictionaryn kaudella pelaavista joukkueista
+"""
+def alusta_joukkueet(kausi):
+    joukkueet = {}
+
+    for game in kausi:
+        for team in game[0:1]:
+            team_name = team[0]
+            joukkueet[team_name] = 0
+
+    return joukkueet
 #%%
 """
 Kohdassa alustetaan pelaajalista p, johon talletetaan pelaajat ja 
@@ -135,7 +209,7 @@ STANDARD_ELO = 1500
 
 for kausi in kaudet:
     for game in kausi:
-        for team in game:
+        for team in game[0:1]:
             team_players = team[2]
             for player_name in team_players:
                 if player_name not in p.getNames():
@@ -155,6 +229,11 @@ for i in range(0,50):
 #%% Ajaa kausien ottelut läpi ja muuttaa pelaajalistan p ELO-painotukset
 #tämän mukaisesti
 
+#p = Players-olio kaikista pelaajista
+#K = ELO-luvun painotuskerroin
+#printOut = tulostetaanko pelin indeksi jossa ollaan menossa
+
+#palauttaa Players-olion p uusilla ELO-arvoilla
 def train(p, kaudet, K, printOut = True):
     for kausi in kaudet:
         #käydään pelit läpi
@@ -164,12 +243,23 @@ def train(p, kaudet, K, printOut = True):
             score_home = game[0][1] 
             score_away = game[1][1]
             
+            isJatko = game[2]
+
             if score_home > score_away:
-                S_a = 3
-                S_b = 0
+                if isJatko:
+                    S_a = 2
+                    S_b = 1
+                else:
+                    S_a = 3
+                    S_b = 0
             else:
-                S_a = 0
-                S_b = 3
+                if isJatko:
+                    S_a = 1
+                    S_b = 2
+                else:
+                    S_a = 0
+                    S_b = 3
+            
             #joukkuekohtaiset ELO-luvut ja kokoonpanot
             home, away, R_a, R_b = team_ELOs(game)
             
@@ -189,36 +279,40 @@ def train(p, kaudet, K, printOut = True):
             
     return p
 
-kaudet = [kausi1415, kausi1516, kausi1617]
+#********** TRAIN **********************************************
+#Määritä kaudet joilla ELO-luku lasketaan, painotuskerroin, 
+#sekä se, resetoidaanko pelaajalista arvoon 1500 ennen opetusta
+#***************************************************************
+kaudet = [kausi1415, kausi1516]
 K = 4
+p.resetELO()
 p = train(p, kaudet, K)
 
 #%% Ennustaa joukkueiden pisteet kaudella pelaajien ELO-arvojen
 #perusteella
 
-def test(p, kausi):
-    """tehdään dictionary joukkueista"""
-    joukkueet = {}
-
-    for game in kausi:
-        for team in game:
-            team_name = team[0]
-            joukkueet[team_name] = 0
+def test(p, kausi, seasonELOs = None):
+    #tehdään dictionary joukkueista
+    joukkueet = alusta_joukkueet(kausi)
            
-    """käydään kauden pelit läpi ja lasketaan niiden odotuspistemäärä"""
+    #käydään kauden pelit läpi ja lasketaan niiden odotuspistemäärä
     for ind, game in enumerate(kausi):
+        home_name = game[0][0]
+        away_name = game[1][0]
+
         #joukkuekohtaiset ELO-luvut ja kokoonpanot
-        home, away, R_a, R_b = team_ELOs(game)
+        if seasonELOs == None:
+            home, away, R_a, R_b = team_ELOs(game)
+        else:
+            R_a = seasonELOs[home_name]
+            R_b = seasonELOs[away_name]
+            
         
         #tuloksen odotusarvo
         E_a = 3/(1 + 10**((R_b - R_a)/400))
         E_b = 3/(1 + 10**((R_a - R_b)/400))
         
-#        score_home = game[0][1] 
-#        score_away = game[1][1]
-        
-        home_name = game[0][0]
-        away_name = game[1][0]
+
 
         joukkueet[home_name] = joukkueet[home_name] + E_a
         joukkueet[away_name] = joukkueet[away_name] + E_b
@@ -226,22 +320,32 @@ def test(p, kausi):
         print(ind)
         
     return joukkueet
-    
-kausi = kausi1718
-joukkueet = test(p, kausi)
-#%% Printtaa joukkueet
-for i in joukkueet:
-    print(i, joukkueet[i])
+#************* TEST ***********************************************
+#Määritä ennustettava kausi, sekä ennusteessa käytettävä ELO-tyyppi
+#seasonELOs = joukkuekohtaiset ELOt lasketaan ennen ennustamista
+#seasonELOs jättämällä pois jokaisen ottelun ELO lasketaan erikseen
+#******************************************************************
+kausi = kausi1617
+seasonELOs = team_season_ELOs(kausi, p)
+joukkueet = test(p, kausi, seasonELOs)
+
 #%% Joukkueet pistejärjestyksessä
 sorted_dict(joukkueet)
 
 #%%
 #laskee seuraavan pelin tuloksen, ja korjaa ELO-arvoa tämän jälkeen 
 #pelin todellisen tuloksen perusteella
+#Antaa parhaita tuloksia, koska ennustus on vain yhden ottelun päähän
 def seuraava_peli(kausi, p):
+    
+    #tehdään dictionary joukkueista
+    joukkueet = alusta_joukkueet(kausi)
+
     oikein = 0
     for i in range(len(kausi)):
-        """ennustetaan pelin tulos joukkueiden ELOjen perusteella"""
+
+
+        #ennustetaan pelin tulos joukkueiden ELOjen perusteella
         game = kausi[i]
         
         score_home = game[0][1] 
@@ -253,14 +357,24 @@ def seuraava_peli(kausi, p):
             or (R_a < R_b and score_home < score_away):
             oikein = oikein + 1
             
-        """muutetaan pelaajien ELO-lukuja todellisen tuloksen perusteella"""
+        #tuloksen odotusarvo, sama kuin test-funktiossa
+        E_a = 3/(1 + 10**((R_b - R_a)/400))
+        E_b = 3/(1 + 10**((R_a - R_b)/400))
+        
+        home_name = game[0][0]
+        away_name = game[1][0]
+
+        joukkueet[home_name] = joukkueet[home_name] + E_a
+        joukkueet[away_name] = joukkueet[away_name] + E_b    
+        
+            
+        #muutetaan pelaajien ELO-lukuja todellisen tuloksen perusteella
         kaudet = [ [kausi[i]] ] #vastaa yhtä peliä ainoastaan
         p = train(p, kaudet, K, printOut = False)
         
     acc = oikein/len(kausi)
-    return acc
+    return acc, joukkueet
 
-kausi = kausi1718
-print(seuraava_peli(kausi, p))
-
-#%%
+kausi = kausi1617
+acc, joukkueet = seuraava_peli(kausi, p)
+print(acc)
--- scaper.py
+++ scaper.py
@@ -2,29 +2,42 @@
 """
 Created on Fri Dec  8 16:54:19 2017
 
-@author: Mikko
+@author: Mikko Impiö
+
 """
 
 import urllib.request as urllib
 from bs4 import BeautifulSoup
-url = "http://liiga.fi/ottelut/2017-2018/runkosarja/1/kokoonpanot/"
+url = "http://liiga.fi/ottelut/2017-2018/runkosarja/6/kokoonpanot/"
 
 def game_players(url):
     page = urllib.urlopen(url)
     
     soup = BeautifulSoup(page, "html.parser")
     
+    #Pelaajat
     h2 = soup.find_all("a", class_="player")
     
+    #Joukkueiden nimet
     [home_name, away_name] = soup.find_all("span", class_="team-name")
     home_name = home_name.get_text().strip()
     away_name = away_name.get_text().strip()
 
+    #Tulokset
     score = soup.find_all("p", class_="score")
     score = score[0].get_text().strip()
     score_h = score[0]
     score_a = score[-1]
-    
+
+    #Tieto jatkoajasta
+    status = soup.find_all("p", class_="status")[0]
+    status = status.get_text()
+    if ("(JA)" or "(VL)") in status:
+        jatko = True
+    else: 
+        jatko = False
+
+    #Pelaajien nimet
     tab = []
     for i in h2:
     
@@ -35,12 +48,18 @@ def game_players(url):
         
     home = tab[0:21]
     away = tab[21:42]
-    tab = [[home_name, score_h, home], [away_name, score_a, away]]
+
+    #Kootaan tiedot yhteen
+    tab = [[home_name, score_h, home], [away_name, score_a, away], jatko]
     return tab
 
 games = []
+
+#Alla liigan verkkosivujen indeksit tiettyjen kausien pelien kokoonpanosivuille
+#2017-2018
 #1,229
-#2016-2017 runko
+
+#2016-2017
 #4147,4596
 
 #2015-2016
@@ -49,9 +68,9 @@ games = []
 #2014-2015
 #6130,6535
 i = 1
-for id in range(1,229):
+for id in range(6130,6535):
 
-    url = "http://liiga.fi/ottelut/2017-2018/runkosarja/" + str(id) + "/kokoonpanot/"
+    url = "http://liiga.fi/ottelut/2014-2015/runkosarja/" + str(id) + "/kokoonpanot/"
     tab = game_players(url)
     games.append(tab) 
     print(str(i) + "/450")
